c6751eb03d6db9d50faf2be65daf8e008dfe5880
Chris Lewis <13chlewis@gmail.com>
2018-12-18 07:47:15 -0700

    Created tests and methods for percentage home wins, percentage visitor wins, average goals per game, and average goals by season

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -1,9 +1,9 @@
 require 'csv'
 
 class StatTracker
-attr_reader :games,
-            :teams,
-            :game_teams
+  attr_reader :games,
+              :teams,
+              :game_teams
   def initialize(games, teams, game_teams)
     @games = games
     @teams = teams
@@ -22,5 +22,52 @@ attr_reader :games,
     teams = CSV.readlines(locations[:teams])
     game_teams = CSV.readlines(locations[:game_teams])
     StatTracker.new(games, teams, game_teams)
+  end
+
+  def percentage_home_wins
+    home_games = []
+    home_wins = []
+    @game_teams.each do |game|
+      if game[2] == "home"
+        home_games << game
+        if game[3] == "TRUE"
+          home_wins << game
+        end
+      end
+    end
+    (home_wins.count.to_f / home_games.count.to_f) * 100.0
+  end
+
+  def percentage_visitor_wins
+    visitor_games = []
+    visitor_wins = []
+    @game_teams.each do |game|
+      if game[2] == "away"
+        visitor_games << game
+        if game[3] == "TRUE"
+          visitor_wins << game
+        end
+      end
+    end
+    (visitor_wins.count.to_f / visitor_games.count.to_f) * 100.0
+  end
+
+  def average_goals_per_game
+    goals_per_game = []
+    @games.each do |game|
+      goals_per_game << (game[6].to_i + game[7].to_i)
+    end
+    goals_per_game.sum.to_f / goals_per_game.count.to_f
+  end
+
+  def average_goals_by_season
+    goals_by_season = Hash.new([])
+    @games.each do |game|
+      goals_by_season[game[1]] += [game[6].to_i + game[7].to_i]
+    end
+    goals_by_season.each do |season_id, goals|
+      goals_by_season[season_id] = (goals.sum.to_f / goals.count.to_f)
+    end
+    goals_by_season
   end
 end
