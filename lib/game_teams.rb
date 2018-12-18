4f81b3c039040656645d044b3a3d1d283cde91a4
Chris Lewis <13chlewis@gmail.com>
2018-12-18 13:45:58 -0700

    Created GameTeams class and refactored StatTracker methods

---
+++ lib/game_teams.rb
@@ -0,0 +1,34 @@
+class GameTeams
+  attr_reader :game_id,
+              :team_id,
+              :hoa,
+              :won,
+              :settled_in,
+              :head_coach,
+              :goals,
+              :shots,
+              :hits,
+              :pim,
+              :powerPlayOpportunities,
+              :powerPlayGoals,
+              :faceOffWinPercentage,
+              :giveaways,
+              :takeaways
+  def initialize(attribute_array)
+    @game_id = attribute_array[0]
+    @team_id = attribute_array[1]
+    @hoa = attribute_array[2]
+    @won = attribute_array[3]
+    @settled_in = attribute_array[4]
+    @head_coach = attribute_array[5]
+    @goals = attribute_array[6]
+    @shots = attribute_array[7]
+    @hits = attribute_array[8]
+    @pim = attribute_array[9]
+    @powerPlayOpportunities = attribute_array[10]
+    @powerPlayGoals =  attribute_array[11]
+    @faceOffWinPercentage = attribute_array[12]
+    @giveaways = attribute_array[13]
+    @takeaways = attribute_array[14]
+  end
+end
