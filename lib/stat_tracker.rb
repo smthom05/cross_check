b94c42ea15780f51e13e04d8a84d853387d9beb9
Trevor Nodland <trevor.nodland@gmail.com>
2018-12-19 15:32:10 -0700

    further refactored methods for average goals by home and away

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -174,28 +174,47 @@ class StatTracker
     team_games_by_id
   end
 
-  def highest_scoring_home_team
+  def generate_scores_by_team_id(csv)
     team_scores_by_id = Hash.new(0)
-    team_games_by_id = generate_number_of_games_by_team_id(game_teams)
 
-    teams.each do |team|
+    csv.each do |team|
       team_scores_by_id[team.team_id] = 0
     end
     team_scores_by_id
+  end
 
-    game_teams.each do |game|
-      if game.hoa == "home"
-        team_scores_by_id[game.team_id] += game.goals
+  def add_goals_by_home_or_away(location, team_scores_by_id)
+    if location == "home"
+      game_teams.each do |game|
+        if game.hoa == "home"
+          team_scores_by_id[game.team_id] += game.goals
+        end
+      end
+    else location == "away"
+      game_teams.each do |game|
+        if game.hoa == "away"
+          team_scores_by_id[game.team_id] += game.goals
+        end
       end
     end
-
     team_scores_by_id
-    average_hash = team_scores_by_id.merge(team_games_by_id){|key, old, new| Array(old).push(new)}
+  end
+
+  def average_goals_per_game_by_team(goals_hash, games_hash)
+    average_hash = goals_hash.merge(games_hash){|key, old, new| Array(old).push(new)}
 
     average_hash.each do |id, value|
       average_hash[id] = value[0].to_f / value[1].to_f
     end
     average_hash
+  end
+
+  def highest_scoring_home_team
+    team_scores_by_id = generate_scores_by_team_id(teams)
+    team_games_by_id = generate_number_of_games_by_team_id(game_teams)
+    goals_at_home = add_goals_by_home_or_away("home", team_scores_by_id)
+    average_hash = average_goals_per_game_by_team(goals_at_home, team_games_by_id)
+
     highest_score = 0
     average_hash.each do |_, score|
       if score > highest_score
@@ -220,20 +239,9 @@ class StatTracker
 
   def lowest_scoring_home_team
     team_games_by_id = generate_number_of_games_by_team_id(game_teams)
-    team_scores_by_id = Hash.new(0)
-
-    game_teams.each do |game|
-      if game.hoa == "home"
-        team_scores_by_id[game.team_id] += game.goals
-      end
-    end
-    team_scores_by_id
-    average_hash = team_scores_by_id.merge(team_games_by_id){|key, old, new| Array(old).push(new)}
-
-    average_hash.each do |id, value|
-      average_hash[id] = value[0].to_f / value[1].to_f
-    end
-    average_hash
+    team_scores_by_id = generate_scores_by_team_id(teams)
+    goals_at_home = add_goals_by_home_or_away("home", team_scores_by_id)
+    average_hash = average_goals_per_game_by_team(goals_at_home, team_games_by_id)
 
     lowest_score = 10.0
     average_hash.each do |_, score|
@@ -260,26 +268,10 @@ class StatTracker
 
   def highest_scoring_visitor
     team_games_by_id = generate_number_of_games_by_team_id(game_teams)
-    team_scores_by_id = Hash.new(0)
+    team_scores_by_id = generate_scores_by_team_id(teams)
+    goals_away = add_goals_by_home_or_away("away", team_scores_by_id)
+    average_hash = average_goals_per_game_by_team(goals_away, team_games_by_id)
 
-    teams.each do |team|
-      team_scores_by_id[team.team_id] = 0
-    end
-    team_scores_by_id
-
-    game_teams.each do |game|
-      if game.hoa == "away"
-        team_scores_by_id[game.team_id] += game.goals
-      end
-    end
-
-    team_scores_by_id
-    average_hash = team_scores_by_id.merge(team_games_by_id){|key, old, new| Array(old).push(new)}
-
-    average_hash.each do |id, value|
-      average_hash[id] = value[0].to_f / value[1].to_f
-    end
-    average_hash
     highest_score = 0
     average_hash.each do |_, score|
       if score > highest_score
@@ -303,20 +295,9 @@ class StatTracker
 
   def lowest_scoring_visitor
     team_games_by_id = generate_number_of_games_by_team_id(game_teams)
-      team_scores_by_id = Hash.new(0)
-
-      game_teams.each do |game|
-        if game.hoa == "away"
-          team_scores_by_id[game.team_id] += game.goals
-        end
-      end
-      team_scores_by_id
-      average_hash = team_scores_by_id.merge(team_games_by_id){|key, old, new| Array(old).push(new)}
-
-      average_hash.each do |id, value|
-        average_hash[id] = value[0].to_f / value[1].to_f
-      end
-      average_hash
+    team_scores_by_id = generate_scores_by_team_id(teams)
+    goals_away = add_goals_by_home_or_away("away", team_scores_by_id)
+    average_hash = average_goals_per_game_by_team(goals_away, team_games_by_id)
 
       lowest_score = 10.0
       average_hash.each do |_, score|
